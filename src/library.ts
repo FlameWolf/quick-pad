db2232b13ed2a9dd622214915350ba22307cbc63
FlameWolf <agnijith@gmail.com>
2026-05-05 11:33:21 +0530

    Fix word count calculation

--- src/library.ts
+++ src/library.ts
@@ -1,6 +1,8 @@
 const sentenceSegmenter = new Intl.Segmenter("en", { granularity: "sentence" });
 const wordSegmenter = new Intl.Segmenter("en", { granularity: "word" });
 const characterSegmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
+const wordMatchRegExp = /[\p{L}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+/u;
+
 export const emptyString = "";
 export const summaryLength = 100;
 export const getSummary = (text: string): string => {
@@ -10,7 +12,7 @@ export const getSentenceCount = (text: string): number => {
 	return Array.from(sentenceSegmenter.segment(text)).length;
 };
 export const getWordCount = (text: string): number => {
-	return Array.from(wordSegmenter.segment(text)).filter(segment => segment.isWordLike).length;
+	return Array.from(wordSegmenter.segment(text)).filter(x => wordMatchRegExp.test(x.segment)).length;
 };
 export const getCharacterCount = (text: string): number => {
 	return Array.from(characterSegmenter.segment(text)).length;
