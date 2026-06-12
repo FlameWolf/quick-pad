01e943a8470eb0ca5dea0407ed9245b20559c6e8
FlameWolf <agnijith@gmail.com>
2026-06-12 14:55:46 +0530

    Separate types with line breaks

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,9 +10,13 @@ interface TypeMap {
 	symbol: symbol;
 	undefined: undefined;
 }
+
 type Json = TypeMap[keyof TypeMap] | Json[] | { [key: keyof any]: Json };
+
 type FromName<T extends keyof TypeMap> = TypeMap[T];
+
 type LegalBlock = { type: "paragraph"; text: string } | { type: "list"; items: string[] };
+
 interface LegalSection {
 	heading: string;
 	blocks: LegalBlock[];
