277b2484908ff8e0ac06ecd92ffc7eaea7e77dbb
FlameWolf <agnijith@gmail.com>
2026-06-18 12:36:59 +0530

    Create icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	import { icons } from "@/constants/icons";
+
+	const props = defineProps<{
+		type: keyof typeof icons;
+	}>();
+</script>
+<template>
+	<template v-html="icons[props.type]"></template>
+</template>
