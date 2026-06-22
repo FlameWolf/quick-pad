9bd4aabcd84974847373649e503a7549ae29456e
FlameWolf <agnijith@gmail.com>
2026-06-22 09:33:40 +0530

    Add store object, setter, and CSS variable for font scaling

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -3,13 +3,20 @@ import { defineStore } from "pinia";
 
 export const useAppStore = defineStore("app", () => {
 	const lastView = ref<View | null>();
+	const fontScaleFactor = ref<number>(0);
 
 	function setLastView(view: View | null) {
 		lastView.value = view;
 	}
 
+	function setFontcaleFactor(factor: number) {
+		fontScaleFactor.value = factor;
+	}
+
 	return {
 		lastView,
-		setLastView
+		fontScaleFactor,
+		setLastView,
+		setFontcaleFactor
 	};
 });
