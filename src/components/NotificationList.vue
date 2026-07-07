5b1b39fd0c267f24e356e5b2f26623e01e32aa6d
FlameWolf <agnijith@gmail.com>
2026-07-07 12:37:36 +0530

    Restyle notifications

--- src/components/NotificationList.vue
+++ src/components/NotificationList.vue
@@ -8,9 +8,9 @@
 	});
 </script>
 <template>
-	<div class="d-flex flex-column gap-1 notification-list position-fixed end-0 bottom-0 me-1 mb-1">
+	<div class="d-flex flex-column notification-list position-fixed end-0 bottom-0 me-2 mb-2">
 		<template v-for="notification in sortedNotifications" :key="notification.id">
-			<div class="alert fade show" :class="`alert-${notification.type}`" role="alert">
+			<div class="alert m-0" :class="`alert-${notification.type}`" role="alert">
 				<div class="d-flex">
 					<div class="me-auto" v-html="notification.message"></div>
 					<button class="btn-close ms-2" @click="notificationsStore.removeNotification(notification.id)" aria-label="Close"></button>
