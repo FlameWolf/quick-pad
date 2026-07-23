77ef15d23a3ea6374f199ba7beab30bc19fc6d00
FlameWolf <agnijith@gmail.com>
2026-07-23 18:27:45 +0530

    Add logic to toggle add tag button

--- src/components/DisplayTagList.vue
+++ src/components/DisplayTagList.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 	import { computed, ref, useTemplateRef } from "vue";
 	import { emptyString } from "@/constants/common";
+	import { normaliseTag } from "@/utils/common";
+	import { contains, equals } from "@/utils/text-analysis";
 	import * as notesStore from "@/stores/notes";
 	import { useDropdown } from "@/composables/useDropdown";
-	import { contains } from "@/utils/text-analysis";
 	import Icon from "@/components/Icon.vue";
 
 	const props = defineProps<{ allowCreate: boolean }>();
@@ -25,6 +26,12 @@
 		return notesStore.tags.value.filter(tag => contains(tag, searchText.value));
 	});
 	const allSelected = computed(() => filteredTags.value.every(tag => selectedTags.value.includes(tag)));
+	const hasExactMatch = computed(() => {
+		if (!searchText) {
+			return true;
+		}
+		return notesStore.tags.value.some(tag => equals(tag, normaliseTag(searchText.value)));
+	});
 
 	function toggleSelectAll() {
 		if (!allSelected.value) {
@@ -43,7 +50,7 @@
 </script>
 <template>
 	<div class="p-1 border rounded mb-3">
-		<button ref="dropdown-toggle" class="btn btn-outline-primary dropdown-toggle" @click="toggle">Tags</button>
+		<button ref="dropdown-toggle" class="btn btn-sm btn-outline-primary dropdown-toggle" @click="toggle">Tags</button>
 		<ul v-if="show" ref="dropdown-menu" class="dropdown-menu show p-2">
 			<li>
 				<label>
@@ -53,9 +60,9 @@
 			</li>
 			<li><hr class="dropdown-divider"/></li>
 			<li>
-				<div class="input-group">
+				<div :class="{ [`input-group`]: allowCreate }">
 					<input v-model.trim="searchText" type="text" class="form-control" placeholder="Search"/>
-					<button class="btn btn-outline-primary">
+					<button v-if="allowCreate" class="btn btn-outline-primary" :disabled="hasExactMatch">
 						<Icon type="plusLg"/>
 					</button>
 				</div>
