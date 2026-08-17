ce9f132e61658c3b740c18db9da04112e9d92e85
FlameWolf <agnijith@gmail.com>
2026-08-17 10:41:08 +0530

    Refactor styles and code

--- src/components/DisplayColourList.vue
+++ src/components/DisplayColourList.vue
@@ -3,6 +3,6 @@
 </script>
 <template>
 	<div class="d-flex flex-wrap gap-2 p-2 border rounded">
-		<a v-for="colour in colours" class="colour-circle border rounded-circle" :class="colour.name.toLowerCase()" role="button" :aria-description="colour.name"></a>
+		<a v-for="colour in colours" class="colour-circle border rounded-circle" :class="`bg-${colour.name.toLowerCase()}`" role="button" :aria-description="colour.name"></a>
 	</div>
 </template>
