32bbc7dd24f528bd136b1f44931afd98d17a9e90
FlameWolf <agnijith@gmail.com>
2026-08-17 10:48:31 +0530

    Emit `selectionChanged` event on clicking a colour

--- src/components/DisplayColourList.vue
+++ src/components/DisplayColourList.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 	import { colours } from "@/constants/colours";
+
+	const emit = defineEmits<{
+		selectionChanged: [colour: string];
+	}>();
 </script>
 <template>
 	<div class="d-flex flex-wrap gap-2 p-2 border rounded">
-		<a v-for="colour in colours" class="colour-circle border rounded-circle" :class="`bg-${colour.name.toLowerCase()}`" role="button" :aria-description="colour.name"></a>
+		<a v-for="colour in colours" class="colour-circle border rounded-circle" :class="`bg-${colour.name.toLowerCase()}`" role="button" @click="emit(`selectionChanged`, colour.name)" :aria-description="colour.name"></a>
 	</div>
 </template>
